d1af5b64cd90f38be3f53d50f16796c1dff39a26
spikespiegel5112 <583288277@qq.com>
2019-08-26 07:34:44 +0800

    added aishopping data

--- backend/routers/testRouters.js
+++ backend/routers/testRouters.js
@@ -5,6 +5,13 @@ const testController = require('../controllers/testController');
 
 
 router.get('/testUrl', testController.testUrl);
+router.get('/getDeals', testController.getDeals);
+router.get('/getBrands', testController.getBrands);
+router.get('/getGifts', testController.getGifts);
+router.get('/getHomepageCarousel', testController.getHomepageCarousel);
+router.get('/getItineraries', testController.getItineraries);
+router.get('/getProducts', testController.getProducts);
+router.get('/getShops', testController.getShops);
 
 
 
